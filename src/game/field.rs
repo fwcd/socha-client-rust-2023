7e32fe72a6360451e78e4fa53dd38af31c0b297d
fwcd <fwcdmail@gmail.com>
2022-08-13 04:10:55 +0200

    Add Field constructors

--- src/game/field.rs
+++ src/game/field.rs
@@ -5,22 +5,40 @@ use super::Team;
 // Port of https://github.com/software-challenge/backend/blob/a3145a91749abb73ca5ffd426fd2a77d9a90967a/plugin/src/main/kotlin/sc/plugin2023/Field.kt
 
 /// A field on the board.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Field {
-    pub fish: usize,
-    pub penguin: Option<Team>,
+    fish: usize,
+    penguin: Option<Team>,
+}
+
+impl Default for Field {
+    fn default() -> Self {
+        Self { fish: 0, penguin: None }
+    }
 }
 
 impl Field {
-    /// Whether the field is empty.
-    pub fn is_empty(&self) -> bool {
-        self.fish == 0 && self.penguin.is_none()
+    /// Creates a new field with the given fish.
+    pub fn with_fish(fish: usize) -> Self {
+        Self { fish, ..Default::default() }
     }
 
-    /// Whether the field is occupied by a penguin.
-    pub fn is_occupied(&self) -> bool {
-        self.penguin.is_some()
+    /// Creates a new field with the given penguin.
+    pub fn with_penguin(penguin: Team) -> Self {
+        Self { penguin: Some(penguin), ..Default::default() }
     }
+
+    /// Whether the field is empty.
+    pub fn is_empty(self) -> bool { self.fish == 0 && self.penguin.is_none() }
+
+    /// Whether the field is occupied by a penguin.
+    pub fn is_occupied(self) -> bool { self.penguin.is_some() }
+
+    /// The number of fish on this field.
+    pub fn fish(self) -> usize { self.fish }
+
+    /// The penguin on this field.
+    pub fn penguin(self) -> Option<Team> { self.penguin }
 }
 
 impl TryFrom<&Element> for Field {
