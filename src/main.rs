d8311262a306b2e62d058892b3f925ab789ef840
fwcd <fwcdmail@gmail.com>
2022-08-13 06:00:18 +0200

    Use clap for argument parsing
    Fixes #1

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
-use std::env;
 use std::str::FromStr;
+use clap::Parser;
 use simplelog::{SimpleLogger, Config};
 use log::LevelFilter;
-use getopts::Options;
 use socha_client_2023::client::{SCClient, DebugMode};
 use socha_client_2023::logic::OwnGameLogic;
 
-fn print_usage(program: &str, options: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("{}", options.usage(&brief));
+/// Software Challenge 2023 client.
+#[derive(Parser, Debug)]
+struct Args {
+    /// The game server's host address.
+    #[clap(short, long, default_value = "localhost")]
+    host: String,
+    /// The game server's port.
+    #[clap(short, long, default_value_t = 13050)]
+    port: u16,
+    /// A game reservation.
+    #[clap(short, long)]
+    reservation: Option<String>,
+    /// The level to log at.
+    #[clap(short, long, default_value = "Info")]
+    level: String,
+    /// Reads incoming XML messages from the console for debugging.
+    #[clap(short = 'd', long)]
+    debug_reader: bool,
+    /// Prints outgoing XML messages to the console for debugging.
+    #[clap(short = 'D', long)]
+    debug_writer: bool,
 }
 
 fn main() {
     // Parse command line arguments
-    let args = env::args().collect::<Vec<_>>();
-    let mut options = Options::new();
-    options.optopt("h", "host", "The game server's host address", "HOST");
-    options.optopt("p", "port", "The game server's port", "PORT");
-    options.optopt("r", "reservation", "A game reservation", "RESERVATION");
-    options.optopt("l", "level", "Optionally provides a custom log level ('Info' by default)", "LEVEL");
-    options.optflag("d", "debug-reader", "Reads incoming XML messages from the console for debugging");
-    options.optflag("D", "debug-writer", "Prints incoming XML messages to the console for debugging");
-    options.optflag("H", "help", "Prints usage info");
+    let args = Args::parse();
     
-    let parsed_args = options.parse(&args[1..]).expect("Could not parse arguments!");
-    if parsed_args.opt_present("help") {
-        print_usage(&args[0], options);
-        return;
-    }
-    
-    let host = parsed_args.opt_str("host").unwrap_or("localhost".to_owned());
-    let port = parsed_args.opt_str("port").unwrap_or("13050".to_owned()).parse::<u16>().expect("Invalid port.");
-    let reservation = parsed_args.opt_str("reservation");
-    let level = parsed_args.opt_str("level").unwrap_or("Info".to_owned());
-    
-    // Setup logging
-    SimpleLogger::init(LevelFilter::from_str(&level).expect("Invalid log level."), Config::default()).expect("Could not initialize logger.");
+    // Set up logging
+    SimpleLogger::init(LevelFilter::from_str(&args.level).expect("Invalid log level."), Config::default()).expect("Could not initialize logger.");
     
     // Setup the client and the delegate
     let debug_mode = DebugMode {
-        debug_reader: parsed_args.opt_present("debug-reader"),
-        debug_writer: parsed_args.opt_present("debug-writer")
+        debug_reader: args.debug_reader,
+        debug_writer: args.debug_writer,
     };
-    let client = SCClient::new(OwnGameLogic, debug_mode, reservation);
-    
-    let _result = client.connect(&host, port).expect("Error while running client.");
+
+    let client = SCClient::new(OwnGameLogic, debug_mode, args.reservation);
+    let _result = client.connect(&args.host, args.port).expect("Error while running client.");
 }
