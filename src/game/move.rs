9e6c69968e69be78a40946fdc22a634052043298
fwcd <fwcdmail@gmail.com>
2022-08-13 04:24:29 +0200

    Add Move methods

--- src/game/move.rs
+++ src/game/move.rs
@@ -5,9 +5,33 @@ use crate::util::{Element, SCError, SCResult};
 use super::Vec2;
 
 /// A game move.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Move {
     from: Option<Vec2>,
     to: Vec2,
+}
+
+impl Move {
+    /// Creates a new move from the given field to the given field.
+    pub fn new(from: Option<Vec2>, to: Vec2) -> Self {
+        Self { from, to }
+    }
+
+    /// Convenience constructor for creating a move of a penguin between two fields.
+    pub fn between(from: Vec2, to: Vec2) -> Self {
+        Self::new(Some(from), to)
+    }
+
+    /// Convenience constructor for creating a move placing a penguin.
+    pub fn placing_at(pos: Vec2) -> Self {
+        Self::new(None, pos)
+    }
+
+    /// The source position if this is a move of an existing penguin, otherwise None.
+    pub fn from(self) -> Option<Vec2> { self.from }
+
+    /// The target position of the penguin.
+    pub fn to(self) -> Vec2 { self.to }
 }
 
 impl TryFrom<&Element> for Move {
